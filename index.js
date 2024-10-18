5a8f1241faab39109b93e3a36a5d1622b3e1374c
S_SAURABH <solankisaurabh2234@gmail.com>
2024-10-18 19:27:31 +0530

    final

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const server = express();
-const mongoose = require("mongoose");
 const path = require("path");
-const userModel = require("./models/userModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const db = require("./config/mongoose_connection");
-
+const { log } = require("console");
+const userModel = require("./models/userModel");
+const productModel = require("./models/productModel");
 
 server.set("view engine", "ejs");
 server.use(express.static(path.join(__dirname, "public")));
@@ -15,29 +15,99 @@ server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 server.use(cookieParser());
 
-server.get("/", (req, res) => {
-    res.render("index");
-})
+// Middleware to verify token and authenticate user
+const authenticateUser = (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) {
+        console.log("No token found, redirecting to login");
+        return res.redirect("/");
+    }
 
-server.post("/", async (req, res) => {
-    let user_2 = await userModel.findOne({ email: req.body.email });
-    if (user_2) return res.status(400).send("User already exists");
-    bcrypt.genSalt(10, async (err, salt) => {
-        bcrypt.hash(req.body.password, salt, async (err, hash) => {
-            let user = await userModel.create({
-                name: req.body.username,
-                email: req.body.email,
-                password: hash
-            })
-            let token = jwt.sign({ email: user.email, userid: user._id }, "secret")
-            res.cookie("token", token);
+    jwt.verify(token, "secret", (err, decoded) => {
+        if (err) {
+            console.log("Invalid token, redirecting to login");
             return res.redirect("/");
-        })
-    })
+        }
+        req.user = decoded; // Store the decoded token data (email, userid) in req.user
+        next(); // Proceed to the next middleware/route handler
+    });
+};
+
+server.get("/", (req, res) => {
+    res.render("index", { isMatch_: 0 });
 })
 
-server.get("/shop", (req, res) => {
-    res.render("shop");
+server.post("/create", async (req, res) => {
+    try {
+        bcrypt.genSalt(10, async (err, salt) => {
+            bcrypt.hash(req.body.password, salt, async (err, hash) => {
+                let user = await userModel.create({
+                    name: req.body.username,
+                    email: req.body.email,
+                    password: hash
+                })
+                let token = jwt.sign({ email: user.email, userid: user._id }, "secret")
+                console.log("signup success");
+                res.cookie("token", token);
+                return res.redirect("/");
+            })
+        })
+    }
+    catch (err) {
+        console.log(err.message);
+    }
+})
+
+server.post("/login", async (req, res) => {
+    try {
+        let user = await userModel.findOne({ email: req.body.email });
+        if (!user) {
+            console.log("User not found");
+        }
+        let isMatch = await bcrypt.compare(req.body.password, user.password);
+        if (isMatch) {
+            let token = jwt.sign({ email: user.email, userid: user._id }, "secret");
+            res.cookie("token", token);
+            console.log("Login success");
+            res.render("index", { isMatch_: 1, name: user.name, email: user.email }); // Redirect on successful login
+        } else {
+            console.log("Invalid credentials");
+        }
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
+server.post("/login/user/product", async (req, res) => {
+    try {
+        let product = await productModel.create({
+            Image: req.body.img,
+            name: req.body.productName,
+            detail_1: req.body.detail_1,
+            detail_2: req.body.detail_2,
+            detail_3: req.body.detail_3,
+            detail_4: req.body.detail_4,
+            detail_5: req.body.detail_5,
+            price: req.body.price
+        });
+        console.log(product);
+        res.redirect("/");
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
+server.get("/logout", (req, res) => {
+    // Clear the authentication cookie
+    res.clearCookie("token");
+    console.log("Logout success");
+    res.render("index", { isMatch_: 0 });
+});
+
+
+server.get("/shop", async (req, res) => {
+    let products = await productModel.find();
+    res.render("shop", { products });
 })
 
 server.get("/pre-build", (req, res) => {
---
+++ models/productModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    Image: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    detail_1: {
+        type: String,
+        required: true
+    },
+    detail_2: {
+        type: String,
+        required: true
+    },
+    detail_3: {
+        type: String,
+        required: true
+    },
+    detail_4: {
+        type: String,
+        required: true
+    },
+    detail_5: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('product', productSchema)
